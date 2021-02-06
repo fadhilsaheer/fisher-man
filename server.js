4d69009ad0524228e63f1f592c3cdbf7087398b5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-02-06 15:47:25 +0530

    created base project

--- server.js
+++ server.js
@@ -1,51 +1,17 @@
-const express = require("express");
-
-const app = express();
-
-let html = `
-<html>
- <style>
-   body{
-       margin: 0;
-       padding: 0;
-       background: #272727;
-       color: #fff;
-       font-family: sans-serif
-   }
-
-   .main{
-       display: flex;
-       justify-content: center;
-       align-items: center;
-       height: 100vh;
-   }
- </style>
- <body>
-   <div class="main">
-     <h1>Happy Hacking 😇</h1>
-   </div>
- </body>
-</html>
-`;
-
-app.get("/", (req, res) => {
-  res.send(html);
-});
-
-app.get("/login", async(req, res) => {
-  let username = await req.query.username;
-  let password = await req.query.password;
-
-  console.log(`${username} : ${password}`); // show username and password in console
-
-  // do whatever with these
-  
-  res.redirect("http://instagram.com");//redirect as your wish
-});
-
-
 // server
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const path = require("path");
+const router = jsonServer.router(path.join(__dirname, "/data/db.json"));
+const middlewares = jsonServer.defaults();
 
-const serverPort = process.env.PORT || 3000;
+// using mii=ddle wares
+server.use(middlewares);
+server.use("/api", router);
+server.use(router);
 
-app.listen(serverPort, () => console.log(`happy-hacking@${serverPort}`));
+// listening server on port
+const SERVER_PORT = process.env.PORT || 3000;
+server.listen(SERVER_PORT, () => {
+  console.log("JSON Server is running");
+});
