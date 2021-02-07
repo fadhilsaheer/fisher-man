40d875c394989103e18ff68a5c4dfaec262d6a15
fadhilsaheer <fadhilsaheer@gmail.com>
2021-02-07 12:44:37 +0530

    signup bug fixed

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,4 +1,8 @@
+import { dbUrl } from './variables.js';
+
 const user = JSON.parse(sessionStorage.getItem("user")); // getting user from session
 
 if(user) window.location.href="./dashboard.html"; // sending user to dashboard page if he already signed in
+
+
 
--- assets/js/signup.js
+++ assets/js/signup.js
@@ -4,64 +4,61 @@ const sessionUser = sessionStorage.getItem("user"); // getting user from session
 
 if (sessionUser) window.location.href = "./dashboard.html"; // sending user to dashboard page if he already signed in
 
+let loader = document.querySelector(".loader-main");
 let form = document.getElementById("form");
-let loader = document.querySelector(".loader-main")
 
 
-form.addEventListener("submit", async (e) => {
 
-  e.preventDefault(); // prevent submitting form
+form.addEventListener("submit", async (formEvent) => {
+  formEvent.preventDefault();
 
-  loader.style.display = "block"
+
+  loader.style.display = "block";
 
   // collecting data
 
-
   let name = document.getElementById("username").value;
   let email = document.getElementById("email").value;
-  let password = new Hashes.MD5().hex(document.getElementById("password").value);
-
+  let password = new Hashes.MD5().hex(
+    document.getElementById("password").value
+  );
 
   const user = { name, email, password };
 
   // check if user exists or not
 
-  let usersData = await fetch(dbUrl + "/users");
-  usersData = await usersData.json();
+  let userDataNameExist = await fetch(`${dbUrl}/users?name=${name}`)
+  let userDataEmailExist = await fetch(`${dbUrl}/users?email=${email}`)
+  userDataNameExist = await userDataNameExist.json()
+  userDataEmailExist = await userDataEmailExist.json()
 
-  let canSignup = true;
+  userDataNameExist = userDataNameExist.length == 0 ? false : true;
+  userDataEmailExist = userDataEmailExist.length == 0 ? false : true
 
-  // looping through element in array
-  
+  let canSignup = false
 
-  for (let i = 0; i < usersData.length; i++) {
-    let userData = usersData[i]
-    if(userData.name == user.name || userData.email == user.email){
-        canSignup = false
-        break;
-    }
-  }
+  if(userDataNameExist == false && userDataEmailExist == false) canSignup = true
 
-  // user signup
+  //   user signup
 
   if (canSignup) {
-
     fetch(dbUrl + "/users", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user)
-    }).then(()=>{
-        sessionStorage.setItem("user", JSON.stringify(user)); // setting user to session 
-        window.location.href = "./dashboard.html"
-
-    })
-
-  }else{
-
-    loader.style.display = "none"
-    swal("Oops something went wrong !!", "look like your username or email is taken 😓", "error")
-
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    }).then(() => {
+      sessionStorage.setItem("user", JSON.stringify(user)); // setting user to session
+      window.location.href = "./dashboard.html";
+    });
+  } else {
+    loader.style.display = "none";
+    swal(
+      "Oops something went wrong !!",
+      "look like your username or email is taken 😓",
+      "error"
+    );
   }
 });
+
