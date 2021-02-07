9f1d3dc0222b3370c49d5dbd4b41b26887c021cc
fadhilsaheer <fadhilsaheer@gmail.com>
2021-02-07 13:02:17 +0530

    login page completed

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,5 +4,39 @@ const user = JSON.parse(sessionStorage.getItem("user")); // getting user from se
 
 if(user) window.location.href="./dashboard.html"; // sending user to dashboard page if he already signed in
 
+let loader = document.querySelector(".loader-main");
+let form = document.getElementById("form");
+
+
+
+form.addEventListener("submit", async (formEvent) => {
+  formEvent.preventDefault();
+
+
+  loader.style.display = "block";
+
+  // collecting data
+
+  let email = document.getElementById("email").value;
+  let password = new Hashes.MD5().hex(
+    document.getElementById("password").value
+  );
+
+  let userData = await fetch(`${dbUrl}/users?email=${email}&password=${password}`);
+  userData = await userData.json();
+
+  let canLogin = userData.length == 0 ? false : true;
+
+  if(canLogin){
+    const user = userData[0]
+    sessionStorage.setItem("user", JSON.stringify(user))
+    window.location.href="./dashboard.html";
+  }else{
+      loader.style.display = "none";
+      swal("I hate invalid login !", "your login info is not correct 🥱", "error")
+  }
+
+
+});
 
 
